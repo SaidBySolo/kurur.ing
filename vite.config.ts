4419bc0b8c10730603d66e880680c17e952fd8b0
Ryu Juheon <saidbysolo@gmail.com>
2023-12-09 03:55:23 +0000

    fix: resolve assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      $fonts: resolve("./src/assets/fonts"),
+      $sprite: resolve("./src/assets/sprite")
+    }
+  },
   plugins: [react()],
 })
