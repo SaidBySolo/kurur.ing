f59af2153ee9797fb9d1c320fea75cfcf9057bc2
Ryu Juheon <saidbysolo@gmail.com>
2023-12-16 02:21:00 +0000

    feat: change to move

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useWindowSize, useInterval } from 'usehooks-ts'
 import "./css/herta.css"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-const Herta = ({ left, top }: { left: number, top: number }) => {
-  return <div className='Herta' style={{
-    left: left,
-    top: top
-  }} />
+// { left, top }: { left: number, top: number }
+
+const widthHerta = 425
+const heightHerta = 340
+
+
+const Herta = ({ x, y }: { x: number, y: number }) => {
+  const styles = {
+    transform: `translate(${x}px, ${y}px)`
+  };
+  return <div className='Herta' style={styles} />
 
 }
 
@@ -16,24 +22,55 @@ const Heading = ({ children = '' }: { children: string }) => {
 
 const App = () => {
   const { width, height } = useWindowSize()
-  const left = Math.random() * (width - 300)
-  const top = Math.random() * (height - 300)
-  const [element, setElement] = useState(<Herta left={left} top={top} />)
+  const [realWidthHerta, setRealWidthHerta] = useState(widthHerta / 1.5)
+  const [realHeightHerta, setRealHeightHerta] = useState(heightHerta / 1.5)
+
+  useEffect(() => {
+    if (width <= 768) {
+      setRealWidthHerta(widthHerta / 3)
+      setRealHeightHerta(heightHerta / 3)
+    } else {
+      setRealWidthHerta(widthHerta / 1.5)
+      setRealHeightHerta(heightHerta / 1.5)
+    }
+  }, [width, height])
+
+  const [transform, setTransform] = useState({
+    x: Math.random() * (realWidthHerta - width) + height,
+    y: Math.random() * (realHeightHerta - height) + width,
+    xSpeed: 1,
+    ySpeed: 1,
+  })
 
   useInterval(
     () => {
-      const left = Math.random() * (width - 300)
-      const top = Math.random() * (height - 300)
+      setTransform(prevState => {
+        const newX = prevState.x + prevState.xSpeed;
+        const newY = prevState.y + prevState.ySpeed;
+        let newXSpeed = prevState.xSpeed;
+        let newYSpeed = prevState.ySpeed;
 
-      setElement(<Herta left={left} top={top} />)
-    },
-    2500
+        if (newX + realWidthHerta / 2 >= width / 2 || newX - realWidthHerta / 2 <= -width / 2) {
+          newXSpeed = -newXSpeed;
+        }
+
+        if (newY + realHeightHerta / 2 >= height / 2 || newY - realHeightHerta / 2 <= -height / 2) {
+          newYSpeed = -newYSpeed;
+        }
+
+        return {
+          x: newX,
+          y: newY,
+          xSpeed: newXSpeed,
+          ySpeed: newYSpeed
+        };
+      });
+    }, 6
   )
-
   return (
     <div className='Container'>
       <Heading>Kuru Kuru Kururing~</Heading>
-      {element}
+      <Herta x={transform.x} y={transform.y} />
     </div>
   )
 }
